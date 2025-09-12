eafb6c8023f4fd67f5d13e3b71d2df88ff6305d9
Matheus Dev <matheushnt.dev@gmail.com>
2025-09-12 14:56:52 -0300

    chore: habilita logging no servidor

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,17 @@
 import fastify from 'fastify'
 import { usersRoutes } from './routes/users'
 
-const app = fastify()
+const app = fastify({
+  logger: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+      },
+    },
+  },
+})
 
 app.register(usersRoutes, {
   prefix: 'users',
