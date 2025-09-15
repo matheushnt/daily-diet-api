361f2328b1b998980f38b6bf1990d5a91b3080ea
Matheus Dev <matheushnt.dev@gmail.com>
2025-09-15 10:58:17 -0300

    test: cria teste de atualização de uma refeição

--- test/meals.spec.ts
+++ test/meals.spec.ts
@@ -117,4 +117,47 @@ describe('Meals routes', () => {
 
     expect(getMealByIdResponse.body.meal.id).toEqual(mealId)
   })
+
+  it('should be able to update a meal', async () => {
+    const createUserResponse = await request(app.server)
+      .post('/users')
+      .send({
+        name: 'Vitor Santos',
+        email: 'vitorsantos@exmaple.com',
+      })
+      .expect(201)
+
+    const cookie = createUserResponse.get('Set-Cookie')
+
+    const createMealResponse = await request(app.server)
+      .post('/meals')
+      .set('Cookie', cookie ?? [])
+      .send({
+        name: 'Salada de fruta',
+        description: 'Durante o almoço, comi uma salada de fruta',
+        isOnDiet: true,
+        datetime: new Date(),
+      })
+      .expect(201)
+
+    const { id: mealId } = createMealResponse.body
+
+    await request(app.server)
+      .put(`/meals/${mealId}`)
+      .set('Cookie', cookie ?? [])
+      .send({
+        name: 'Torta de limão',
+        description: 'Comi uma torta de limão com a minha vó',
+        isOnDiet: false,
+        datetime: new Date(Date.now() + 1000 * 60 * 60 * 24), // Um dia depois
+      })
+
+    const getMealByIdResponse = await request(app.server)
+      .get(`/meals/${mealId}`)
+      .set('Cookie', cookie ?? [])
+      .expect(200)
+
+    expect(getMealByIdResponse.body.meal).toHaveProperty('name', 'Torta de limão')
+    expect(getMealByIdResponse.body.meal).toHaveProperty('description', 'Comi uma torta de limão com a minha vó')
+  })
 })
